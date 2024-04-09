f7831d243a83b430dea5b0b1198889de30cdbf7d
summergua <1363106651@qq.com>
2024-04-09 22:51:00 +0800

    feat: pinia

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
-import { createApp } from "vue";
-import "./style.css";
-import App from "./App.vue";
-import router from "@/router";
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
+import router from '@/router';
+import store from '@/store';
+
 const app = createApp(App);
-app.use(router);
-app.mount("#app");
+app.use(store).use(router);
+app.mount('#app');
