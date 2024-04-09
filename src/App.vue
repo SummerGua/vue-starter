a1cc7e8e92eb23ab0e7c70b80eb2c74e77187174
summergua <1363106651@qq.com>
2024-04-09 22:47:33 +0800

    init: 脚手架初始化

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+  <div class="w-full flex justify-around text-blue bg-[#f5f5f5]">
+    <router-link to="/">to HOME</router-link>
+    <router-link to="/about">to ABOUT</router-link>
+  </div>
+  <router-view></router-view>
+</template>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+defineProps<{ msg: string }>();
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+  <p class="text-xl text-red-400">tailwindCSS availble</p>
+</template>
